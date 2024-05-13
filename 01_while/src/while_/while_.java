29ab8e5cbed964c0389c3cc86c4b9a96a9b7ae76
unknown <paulocardosoassessor@gmail.com>
2024-05-13 13:27:28 -0300

    While e Do While

---
+++ 01_while/src/while_/while_.java
@@ -0,0 +1,16 @@
+package while_;
+
+public class while_ {
+
+	public static void main(String[] args) {
+
+		int numero = 0;
+
+		// Priemiro verifica depois executa
+		while (numero < 10) {
+			numero++;
+
+			System.out.println("O numero atual é: " + numero);
+		}
+	}
+}
---
+++ 02_do_while/src/do_while_/do_while_.java
@@ -0,0 +1,15 @@
+package do_while_;
+
+public class do_while_ {
+
+	public static void main(String[] args) {
+
+		int numero = 0;
+
+		// Primeiro executa depois verifica
+		do {
+			System.out.println("O numero atual é: " + numero);
+			numero++;
+		} while (numero <= 10);			
+	}
+}
