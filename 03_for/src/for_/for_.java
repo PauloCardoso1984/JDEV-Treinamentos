f2b81baa27fe41a7ac88cde705cbe6ca08dd199e
unknown <paulocardosoassessor@gmail.com>
2024-05-13 13:35:59 -0300

    Estrutura de repetição FOR - CRESCENTE

---
+++ 03_for/src/for_/for_.java
@@ -0,0 +1,18 @@
+package for_;
+
+public class for_ {
+
+	public static void main(String[] args) {
+		
+		// Estrutura de repetição FOR
+		
+		for (int numero = 0; numero <= 10; numero++) {	
+			System.out.println("Numero atual é: " + numero);
+		}
+		System.out.println();
+		
+		for (int i = 0; i <= 25; i++) {	
+			System.out.println("Numero atual é: " + i);
+		}
+	}
+}
