56afac40c3349c0d3f678e7f3c71765f1dbfc53d
unknown <paulocardosoassessor@gmail.com>
2024-05-21 13:18:56 -0300

    Método toString

--- 12_metodo_toString/src/classes/curso/executa/Programa.java
+++ 12_metodo_toString/src/classes/curso/executa/Programa.java
@@ -14,19 +14,19 @@ public class Programa {
 		System.out.print("Qual o nome do aluno: ");
 		aluno1.nome = sc.nextLine();
 		
-		System.out.print("Qual é a idade: ");
+		System.out.print("Qual é a idade: ");	
 		aluno1.idade = sc.nextInt();
 		
-		System.out.print("Qual a data de nascimento: ");
+		System.out.print("Qual a data de nascimento: ");	
 		aluno1.dataNascimento = sc.next();
 		
-		System.out.print("Numero do RG: ");
+		System.out.print("Numero do RG: ");	
 		aluno1.registroGeral = sc.next();
 		
 		System.out.print("Numero do CPF: ");
 		aluno1.numeroCpf = sc.next();
 		
-		System.out.print("Nome da mãe: ");
+		System.out.print("Nome da mãe: ");	
 		aluno1.nomeMae = sc.next();
 		
 		System.out.print("Nome do pai: ");
@@ -35,12 +35,12 @@ public class Programa {
 		System.out.print("Data da matrícula: ");
 		aluno1.dataMatricula = sc.next();
 		
-		System.out.print("Série que está cursando: ");
+		System.out.print("Série que está cursando: ");	
 		aluno1.serieMatriculado = sc.next();
 		
-		System.out.print("Nome da escola: ");
+		System.out.print("Nome da escola: ");		
 		aluno1.nomeEscola = sc.next();
-
+		
 		System.out.println("Informe as 4 notas: ");
 		System.out.print("1a nota: ");
 		aluno1.setNota1(sc.nextDouble());
@@ -50,15 +50,11 @@ public class Programa {
 		aluno1.setNota3(sc.nextDouble());
 		System.out.print("4a nota: ");
 		aluno1.setNota4(sc.nextDouble());
-
-		System.out.println("\nNome: 		" + aluno1.getNome());
-		System.out.println("Nascimento: 	" + aluno1.getDataNascimento());
-		System.out.println("Média: 		" + aluno1.getMediaNota());
-		System.out.println("Situação1: 	" + aluno1.getSituacaoAluno());
-		System.out.println("Situação2: 	" + (aluno1.getSituacaoAluno2() ? "Aprovado" : "Reprovado")); // OPERADOR TERNÁRIO
 		
 		System.out.println("====================================");
 
+		System.out.println(aluno1.toString());
+		
 		sc.close();
 	}
 }
